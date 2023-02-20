4d26ba267cb0d826918ae7032a33cf6e526e12d7
Pablo Esteban Salazar Marquez <pablo.salazar@ibm.com>
2023-02-20 13:30:56 -0600

    first commit, create a Spring Boot app

---
+++ src/test/java/com/ibm/webservice/restfulwebservices/ResfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.webservice.restfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
