1e3f5b9dc095da9b2c7b0a0b188c4ca3088061ff
Pablo Esteban Salazar Marquez <p.e.salazarmarquez@gmail.com>
2023-02-20 19:19:54 -0600

    Add CustomizedResponseEntityExceptionHandler Add ExceptionResponse Add UserNotFoundException
    Modified GetMapping and added UserNotFoundException for errors when user not found, message Http status response and details about request

--- src/main/java/com/ibm/webservice/restfulwebservices/user/UserResource.java
+++ src/main/java/com/ibm/webservice/restfulwebservices/user/UserResource.java
@@ -15,20 +15,23 @@ public class UserResource {
     private UserDaoService service;
 
     @GetMapping("/users")
-    public List<User> retrieveAllUsers(){
+    public List<User> retrieveAllUsers() {
         return service.findAll();
     }
 
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id){
-        return service.findOne(id);
+    public User retrieveUser(@PathVariable int id) {
+        User user = service.findOne(id);
+        if (user == null)
+            throw new UserNotFoundException("id-" + id);
+        return user;
     }
 
-    //CREATED
+    //
     //input - details of user
     //output - CREATED & Return the created URI
     @PostMapping("/users")
-    public void createUser(@RequestBody User user){
+    public void createUser(@RequestBody User user) {
         User savedUser = service.save(user);
 
         URI location = ServletUriComponentsBuilder
