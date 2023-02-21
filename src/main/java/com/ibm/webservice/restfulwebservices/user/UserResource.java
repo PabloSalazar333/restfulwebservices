f08ac622e26d1a6a9d39ed4ca8628e7c41e00fad
Pablo Esteban Salazar Marquez <p.e.salazarmarquez@gmail.com>
2023-02-20 18:20:29 -0600

    Add Response Entity on Post request

--- src/main/java/com/ibm/webservice/restfulwebservices/user/UserResource.java
+++ src/main/java/com/ibm/webservice/restfulwebservices/user/UserResource.java
@@ -1,8 +1,11 @@
 package com.ibm.webservice.restfulwebservices.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -27,5 +30,11 @@ public class UserResource {
     @PostMapping("/users")
     public void createUser(@RequestBody User user){
         User savedUser = service.save(user);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId()).toUri();
+        ResponseEntity.created(location).build();
     }
 }
